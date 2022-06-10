4d9484b1cb813c52d8c896436e2bd41118c23cb5
Mary Stankevich <marrycitty@mail.ru>
2022-06-10 17:53:38 +0200

    Update Task_4.java

--- src/Homework_2/Task_4/Task_4.java
+++ src/Homework_2/Task_4/Task_4.java
@@ -1,10 +1,11 @@
 package Homework_2.Task_4;
 
+//+-
 public class Task_4 {
     public static void main(String[] args) {
         int a = 7;
         int b = 7;
-        int c = 0;
+        int c = 0; //название переменной должно быть более значащим (например, result)
 
         for (int i = 1; i <= b; i++) {
             c = c + a;
