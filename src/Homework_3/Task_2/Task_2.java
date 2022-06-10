2c0bbb1688642072a032cc6d0b359fff9e636ffa
Mary Stankevich <marrycitty@mail.ru>
2022-06-10 18:00:54 +0200

    Update Task_2.java

--- src/Homework_3/Task_2/Task_2.java
+++ src/Homework_3/Task_2/Task_2.java
@@ -1,10 +1,11 @@
 package Homework_3.Task_2;
 
+//+
 public class Task_2 {
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 4, 5};
         int sum = 0;
-        int average = 0;
+        int average = 0; //можно перенести ближе к использованию и в той же строчки инициализировать рассчитанным значением
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
