e1ee2e2607c7aa55fac89b6101adab854f162580
Mary Stankevich <marrycitty@mail.ru>
2022-06-10 17:46:57 +0200

    Update HelloMikhail.java

--- src/Homework_1/Task_1/HelloMikhail.java
+++ src/Homework_1/Task_1/HelloMikhail.java
@@ -1,3 +1,5 @@
+//+- названия пакетов должны быть маленькими буквами
+//Мне кажется, что не хватает в первой строчки класса package
 public class HelloMikhail {
     public static void main(String[] args)  {
         System.out.println("Hello Mikhail");
