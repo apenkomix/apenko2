7452838d47c36fb4c4f0efc650c73e0f96d6e04a
Mary Stankevich <marrycitty@mail.ru>
2022-06-10 18:04:15 +0200

    Update Task_5.java

--- src/Homework_3/Task_5/Task_5.java
+++ src/Homework_3/Task_5/Task_5.java
@@ -1,9 +1,10 @@
 package Homework_3.Task_5;
 
+//+
 public class Task_5 {
     public static void main(String[] args) {
         int[] array = new int[]{0, 1, 2, 3, 4};
-        int order = 0;
+        int order = 0; //что значит имя переменной? Как я вижу, это буфер для перекидки элементов. Может быть temp, buff, buffer  
         for (int i = 0; i < array.length / 2; i++) {
             order = array[i];
             array[i] = array[array.length - i - 1];
