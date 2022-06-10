60a171b9f2504e0ec2efc45022dfb0dc40a31a0b
Mary Stankevich <marrycitty@mail.ru>
2022-06-10 18:06:21 +0200

    Update Task_6.java

--- src/Homework_3/Task_6/Task_6.java
+++ src/Homework_3/Task_6/Task_6.java
@@ -3,10 +3,10 @@ package Homework_3.Task_6;
 public class Task_6 {
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] array1 = new int[(array.length) / 2];
+        int[] array1 = new int[(array.length) / 2]; //попробуй подобрать более значащее название переменной
         for (int i = 0; i < array.length; i += 2) {
             array1[i/2] = array[i] + 1;
-            System.out.println(array1[i/2]);
+            System.out.println(array1[i/2]); //стоит вывести весь массив сразу, а не поэлементно, чтобы показать, что там лежат все нужные элементы
         }
     }
 }
