bcc58fe53544562b585a8bc74796b0dac33c7960
Mary Stankevich <marrycitty@mail.ru>
2022-07-06 19:32:58 +0200

    Update Main.java

--- src/Homework_10/Task_1/Main.java
+++ src/Homework_10/Task_1/Main.java
@@ -1,9 +1,11 @@
 package Homework_10.Task_1;
 
+//+-
 public class Main {
     public static void main(String[] args) {
         Summer<Integer> summer = new Summer<>(22, "class");
+        //метод должен быть у класса Summer
         System.out.println(summer.getComment().getClass().getName());
-
+//лишняя строчка
     }
 }
