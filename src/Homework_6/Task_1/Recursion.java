24bedda937ea18614c4e3795d0f3171dba4c7088
Mary Stankevich <marrycitty@mail.ru>
2022-07-06 23:15:48 +0200

    Update Recursion.java

--- src/Homework_6/Task_1/Recursion.java
+++ src/Homework_6/Task_1/Recursion.java
@@ -1,11 +1,13 @@
 package Homework_6.Task_1;
 
+//+
 public class Recursion {
     public static void main(String[] args) {
         Integer [] array = new Integer[]{1, 2, 3, 4, 5, 6};
         calculate(array,0);
     }
 
+    //название метода может ввести в заблуждение - метод не считает, просто выводит
     public static void calculate(Integer[] array,Integer number) {
         if (number >= array.length) {
             return;
