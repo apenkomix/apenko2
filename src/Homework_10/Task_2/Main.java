89d6b864cd62e1d0ef29736ac09274a945f3552e
Mary Stankevich <marrycitty@mail.ru>
2022-07-06 19:43:49 +0200

    Update Main.java

--- src/Homework_10/Task_2/Main.java
+++ src/Homework_10/Task_2/Main.java
@@ -1,5 +1,6 @@
 package Homework_10.Task_2;
 
+//+- Посмотри комментарий в другом классе и попробуй переписать
 public class Main {
     public static void main(String[] args) {
         Direct files = Direct.FILES;
