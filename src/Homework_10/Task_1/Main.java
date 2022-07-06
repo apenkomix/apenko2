10c7eaeca78be8ccb83edef7630e61a034d8c0aa
Mary Stankevich <marrycitty@mail.ru>
2022-07-06 19:50:50 +0200

    Update Main.java

--- src/Homework_10/Task_1/Main.java
+++ src/Homework_10/Task_1/Main.java
@@ -1,6 +1,6 @@
 package Homework_10.Task_1;
 
-//+-
+//+- Кстати, названия пакетов пишутся маленькими буквами (snake_case)
 public class Main {
     public static void main(String[] args) {
         Summer<Integer> summer = new Summer<>(22, "class");
