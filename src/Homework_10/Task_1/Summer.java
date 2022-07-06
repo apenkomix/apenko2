cf46055fa47063dd869da307e5acd71bc56bd3c7
Mary Stankevich <marrycitty@mail.ru>
2022-07-06 19:32:00 +0200

    Update Summer.java

--- src/Homework_10/Task_1/Summer.java
+++ src/Homework_10/Task_1/Summer.java
@@ -1,5 +1,8 @@
 package Homework_10.Task_1;
 
+//+-
+//код не отформатирован
+//но много из изученного использовано, молодец)
 public class Summer <T> {
     private T day;
     private String comment;
@@ -16,4 +19,5 @@ public class Summer <T> {
     public String getComment() {
         return comment;
     }
+    //Не хватает метода который бы выводил что это за тип на экран (например, printType - в твоем случае можно вывести 2, так что даже printTypes)
 }
