6f835b8596c32005b4623c1b2f412fce91456069
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 12:14:53 +0200

    Update Engeneering.java

--- src/Homework_7/Task_1/Engeneering.java
+++ src/Homework_7/Task_1/Engeneering.java
@@ -1,10 +1,11 @@
 package Homework_7.Task_1;
 
+//+
 public class Engeneering {
     public static void main(String[] args){
         String engeneering = "engineering";
         System.out.println(engeneering.substring(0,6));
         System.out.println(engeneering.substring(7,11));
-
+//Лишняя пустая строка 
     }
 }
