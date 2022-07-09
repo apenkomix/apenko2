5d2fd4243dbd32e58e64b85fbc7833436840ffb1
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 12:46:30 +0200

    Update Main.java

--- src/Homework_9/Task_4/Main.java
+++ src/Homework_9/Task_4/Main.java
@@ -4,6 +4,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
+//+
 public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee("Mikhail", new Work("Java Developer", 1), 29);
