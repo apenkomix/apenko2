5149a6258309f135ee1a02456a6037290be4f485
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 12:54:04 +0200

    Update Directory.java

--- src/Homework_9/Task_7/Directory.java
+++ src/Homework_9/Task_7/Directory.java
@@ -2,6 +2,7 @@ package Homework_9.Task_7;
 
 import java.io.File;
 
+//+- не печатает вложенные. Решается через рекурсию,попробуй ещё. Если не получится, напиши мне, скину решение 
 public class Directory {
     public static void main(String[] args) {
        final File folder = new File("src");
