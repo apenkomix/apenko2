9f9ade3fb1c46eb00decd12f75f957e08c513466
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 12:45:16 +0200

    Update Employee.java

--- src/Homework_9/Task_4/Employee.java
+++ src/Homework_9/Task_4/Employee.java
@@ -2,7 +2,7 @@ package Homework_9.Task_4;
 
 import java.io.Serializable;
 
-//+
+//+ Только не хватает private static final long serialVersionUID, чтобы (де)сериализация не ломалась при изменении класса
 public class Employee implements Serializable {
     private String name;
     private Work work;
