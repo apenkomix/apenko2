f1bcff1b117b711ed94736a310a227b2de98e312
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 12:32:46 +0200

    Update Main.java

--- src/Homework_8/Task_3/Main.java
+++ src/Homework_8/Task_3/Main.java
@@ -18,9 +18,11 @@ public class Main {
                 Integer[] array = new Integer[arraySize];
                 break;
             } catch (SizeLimitException e) {
+                //Тут тоже стоит использовать значение переменной maxLength - избегаем "магических цифр"
                 System.out.println("Max length 20");
             } catch (InputMismatchException exception){
                 System.out.println("Type number pls");
+                //Если тут сделать брейк, пользователь не сможет ещё раз попробовать ввести значение
                 break;
             }
         }
