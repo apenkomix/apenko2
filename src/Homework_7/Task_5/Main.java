3e02e19eaefb661099371140b7d40e8055690a09
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 12:21:58 +0200

    Update Main.java

--- src/Homework_7/Task_5/Main.java
+++ src/Homework_7/Task_5/Main.java
@@ -1,9 +1,10 @@
 package Homework_7.Task_5;
 
+//+
 public class Main {
     public static void main(String[] args) {
         String strArray = "well,good,bad, will, Wood, bear";
-        String[] array = strArray.split("\\W");
+        String[] array = strArray.split("\\W"); //клёвый сплит, его же можно было использовать в задании 4
         for (String s : array) {
             if (s.toLowerCase().startsWith("w")) {
                 System.out.println(s);
