eb97e99c04b898ff2db590f0e8ea01d8fa759078
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 12:50:53 +0200

    Update Main.java

--- src/Homework_9/Task_7/Main.java
+++ src/Homework_9/Task_7/Main.java
@@ -5,6 +5,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+// Хорошо, что погуглил) после занятия 13 ты будешь больше понимать, что тут происходит ;)
 public class Main {
     public static void main(String[] args) {
         try (Stream<Path> paths = Files.walk(Paths.get("src"))) {
