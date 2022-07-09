f0175fa9a31868cbd84f2db051e15593dd2b05a1
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 12:19:10 +0200

    Update Alucard.java

--- src/Homework_7/Task_2/Alucard.java
+++ src/Homework_7/Task_2/Alucard.java
@@ -1,11 +1,14 @@
 package Homework_7.Task_2;
 
+//+
 public class Alucard {
     public static void main(String[] args) {
         String alucard = "alucard";
+        //Тут он ещё не реверс, а просто массив из строки
         char[] reversArray = alucard.toCharArray();
         for (int i = reversArray.length - 1; i >= 0; i--) {
-
+            //Идея хороша, но значение никуда не сохраняется
+            //Можно использовать String.at() и закидывать в пустой reversArray, потом из него сделать строку
             System.out.print(reversArray[i]);
         }
     }
