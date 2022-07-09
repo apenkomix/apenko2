b8affe9c585f09c867bc11402e54a63718dea0a1
Mary Stankevich <marrycitty@mail.ru>
2022-07-09 12:27:06 +0200

    Update Main.java

--- src/Homework_7/Task_8/Main.java
+++ src/Homework_7/Task_8/Main.java
@@ -1,7 +1,9 @@
 package Homework_7.Task_8;
 
+//+
 public class Main {
     public static void main(String[] args) {
+        //string -> srings, stringArray, потому что строка в переменной не одна
         String[] string = {"Rachel", "Mickhail", "Rob", "Leo", "Alexsander", "Mila","Richy"};
         for (int i = 0; i < string.length; i++) {
             if (string[i].length() > 5) {
